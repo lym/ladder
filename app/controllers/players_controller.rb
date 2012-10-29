adb108e04608c2330af5455fbd2c93c2113c98b5
Salym Senyonga <salymsash@gmail.com>
2012-10-29 03:38:33 +0300

    Fixed bug in find_winner action

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,6 @@
 class PlayersController < ApplicationController
   def index
+    @players = Player.all
   end
 
 
@@ -19,11 +20,24 @@ class PlayersController < ApplicationController
   def find_winner
     @player_1 = params[:player_1]
     @player_2 = params[:player_2]
-    @set_1 = params[:set_1]
-    @set_2 = params[:set_2]
-    @set_3 = params[:set_3]
+    @set_1 = params[:set_1].split(',').map(&:to_i)
+    @set_2 = params[:set_2].split(',').map(&:to_i)
+    @set_3 = params[:set_3].split(',').map(&:to_i)
 
-    @a = compute_winner(@player_1, @player_2, @set_1, @set_2, @set_3) and return
+    a = @set_1[0].to_i
+    b = @set_1[1].to_i
+    c = @set_2[0].to_i
+    d = @set_2[1].to_i
+    e = @set_3[0].to_i
+    f = @set_3[1].to_i
+
+    if ((a + c + e) > (b + d + f)) then 
+      @winner = @player_1
+    elsif ((a + c + e) < (b + d + f)) then 
+      @winner = @player_2
+    else
+      @winner = "draw"
+    end
   end
 
   def show
@@ -31,17 +45,5 @@ class PlayersController < ApplicationController
 
    protected 
     def compute_winner(player_1, player_2, set_1, set_2, set_3=[0,0])
-      a = set_1[0] 
-      b = set_1[1] 
-      c = set_2[0] 
-      d = set_2[1]
-      e = set_3[0]
-      f = set_3[1]
-
-      if (a + c + e) > (b + d + f) then render :text => "winner is #{player_1}" and return
-      elsif (a + c + e) < (b + d + f) then render :text => "winner is #{player_2}" and return
-      else
-        render :text => "sorry man, there're no draws in tennis" and return
-      end
     end
 end
